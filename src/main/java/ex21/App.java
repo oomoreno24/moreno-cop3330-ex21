0e4c716928ca0e8660de31ac9b3be2a5f600d00a
Oliver Moreno <olivermoreno@knights.ucf.edu>
2021-06-06 17:38:54 -0400

    Initial solution to problem 21

--- src/main/java/ex21/App.java
+++ src/main/java/ex21/App.java
@@ -30,6 +30,53 @@ Support multiple languages. Prompt for the language at the beginning of the prog
 
 public class App {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter the number of the month: ");
+        String monthInput = in.nextLine();
+
+        // convert string to integer
+        int month = Integer.parseInt(monthInput);
+
+        switch (month) {
+            case 1:
+                System.out.println("The name of the month is January.");
+                break;
+            case 2:
+                System.out.println("The name of the month is February.");
+                break;
+            case 3:
+                System.out.println("The name of the month is March.");
+                break;
+            case 4:
+                System.out.println("The name of the month is April.");
+                break;
+            case 5:
+                System.out.println("The name of the month is May.");
+                break;
+            case 6:
+                System.out.println("The name of the month is June.");
+                break;
+            case 7:
+                System.out.println("The name of the month is July.");
+                break;
+            case 8:
+                System.out.println("The name of the month is August.");
+                break;
+            case 9:
+                System.out.println("The name of the month is September.");
+                break;
+            case 10:
+                System.out.println("The name of the month is October.");
+                break;
+            case 11:
+                System.out.println("The name of the month is November.");
+                break;
+            case 12:
+                System.out.println("The name of the month is December.");
+                break;
+            default:
+                System.out.println("Error, number does not correspond to a month.");
+        }
+
     }
 }
